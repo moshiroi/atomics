a1a9f0cc72d3afbf1ac76c2cfa3df8bd79831bb4
moshiroi <mqsas1337@gmail.com>
2024-12-23 09:24:18 +0000

    del target

--- channels/target/debug/deps/channels-7d14cdac8d09aa73.d
+++
@@ -1,9 +0,0 @@
-/Users/mohamedshire/git/atomics/channels/target/debug/deps/libchannels-7d14cdac8d09aa73.rmeta: src/main.rs Cargo.toml
-
-/Users/mohamedshire/git/atomics/channels/target/debug/deps/channels-7d14cdac8d09aa73.d: src/main.rs Cargo.toml
-
-src/main.rs:
-Cargo.toml:
-
-# env-dep:CLIPPY_ARGS=
-# env-dep:CLIPPY_CONF_DIR
