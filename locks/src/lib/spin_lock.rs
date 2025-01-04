a272087f6967f780f74f58eb17be624c097507d2
moshiroi <mqsas1337@gmail.com>
2025-01-04 20:41:07 +0300

    rename spin-lock -> lock, add git ignore, refactor

--- locks/src/lib/spin_lock.rs
+++ locks/src/lib/spin_lock.rs
@@ -2,30 +2,7 @@ use std::{
     cell::UnsafeCell,
     ops::{Deref, DerefMut},
     sync::atomic::{AtomicBool, Ordering},
-    thread,
 };
-
-fn main() {
-    let spin_lock: &'static _ = Box::leak(Box::new(SpinLock::new(0)));
-
-    let mut threads = vec![];
-    for _ in 0..10 {
-        let t = thread::spawn(|| {
-            let mut g = spin_lock.lock();
-            for _ in 0..25 {
-                *g += 1;
-            }
-        });
-
-        threads.push(t);
-    }
-
-    for thread in threads {
-        thread.join().unwrap();
-    }
-
-    assert_eq!(*spin_lock.lock(), 250);
-}
 
 pub struct SpinLock<T> {
     lock: AtomicBool,
