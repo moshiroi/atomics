045566589db783ba71f0843ccc6089247866c6ab
moshiroi <mqsas1337@gmail.com>
2025-01-07 20:52:44 +0300

    reworked project structure to cargo workspace + lib

---
+++ locks/src/old-main.rs
@@ -0,0 +1,21 @@
+fn main() {
+    // let spin_lock: &'static _ = Box::leak(Box::new(SpinLock::new(0)));
+
+    // let mut threads = vec![];
+    // for _ in 0..10 {
+    //     let t = thread::spawn(|| {
+    //         let mut g = spin_lock.lock();
+    //         for _ in 0..25 {
+    //             *g += 1;
+    //         }
+    //     });
+
+    //     threads.push(t);
+    // }
+
+    // for thread in threads {
+    //     thread.join().unwrap();
+    // }
+
+    // assert_eq!(*spin_lock.lock(), 250);
+}
--- locks/src/spin_lock.rs
+++ locks/src/spin_lock.rs
@@ -63,3 +63,32 @@ impl<T> Drop for Guard<'_, T> {
         self.lock.lock.store(false, Ordering::Release)
     }
 }
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+    use std::thread;
+    #[test]
+    fn to_10000() {
+        let spin_lock: &'static _ = Box::leak(Box::new(SpinLock::new(0)));
+
+        let mut threads = vec![];
+        for _ in 0..10 {
+            let t = thread::spawn(|| {
+                let mut g = spin_lock.lock();
+                for _ in 0..25 {
+                    *g += 1;
+                }
+            });
+
+            threads.push(t);
+        }
+
+        for thread in threads {
+            thread.join().unwrap();
+        }
+
+        assert_eq!(*spin_lock.lock(), 250);
+    }
+}
