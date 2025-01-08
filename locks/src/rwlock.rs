c3ef42f7194932a05c2a5d881b4613b913178ac9
moshiroi <mqsas1337@gmail.com>
2025-01-08 19:29:41 +0300

    working impl of RwLock?

--- locks/src/rwlock.rs
+++ locks/src/rwlock.rs
@@ -1,27 +1,57 @@
 use std::{
     cell::UnsafeCell,
     ops::{Deref, DerefMut},
-    sync::atomic::AtomicU32,
+    sync::atomic::{AtomicU32, Ordering},
 };
+
+use atomic_wait::{wait, wake_all, wake_one};
 
 pub struct RwLock<T> {
     state: AtomicU32,
     value: UnsafeCell<T>,
-    waiting_writers: AtomicU32,
+    writer_beacon: AtomicU32,
 }
 
 impl<T> RwLock<T> {
     fn read(&self) -> ReadGuard<T> {
-        // If state is even, state += 2
-        // If state is odd, you must not dish out any further readers. wait.
-        todo!()
+        // NOTE: Concerned that state may change between the load + processing operations.
+        // Not entirely atomic
+
+        // CAS operation after the state.load() addresses the above concerns
+        let mut s = self.state.load(Ordering::Acquire);
+
+        loop {
+            if s < u32::MAX {
+                assert_ne!(s, u32::MAX - 1, "Too many readers");
+
+                match self
+                    .state
+                    .compare_exchange(s, s + 1, Ordering::Acquire, Ordering::Relaxed)
+                {
+                    Ok(_) => return ReadGuard { lock: self },
+                    Err(e) => s = e,
+                }
+
+                if s == u32::MAX {
+                    wait(&self.state, u32::MAX);
+                    s = self.state.load(Ordering::Acquire);
+                }
+            }
+        }
     }
 
     fn write(&mut self) -> WriteGuard<T> {
-        // If state is odd, there is already a writer waiting - sleep
-        // If state is even, state += 1, wait
-        // If state == 1, return WriteGuard
-        todo!()
+        while let Err(e) =
+            self.state
+                .compare_exchange(0, u32::MAX, Ordering::Acquire, Ordering::Relaxed)
+        {
+            let writer_beacon = self.state.load(Ordering::Acquire);
+            if self.state.load(Ordering::Acquire) != 0 {
+                wait(&self.writer_beacon, writer_beacon)
+            }
+        }
+
+        WriteGuard { lock: self }
     }
 }
 
@@ -38,7 +68,13 @@ impl<'a, T> Deref for ReadGuard<'a, T> {
 }
 
 impl<T> Drop for ReadGuard<'_, T> {
-    fn drop(&mut self) {}
+    fn drop(&mut self) {
+        if self.lock.state.fetch_sub(1, Ordering::Acquire) == 1 {
+            // Wake a potential waiting writer
+            self.lock.writer_beacon.fetch_add(1, Ordering::Release);
+            wake_one(&self.lock.writer_beacon);
+        }
+    }
 }
 
 struct WriteGuard<'a, T> {
@@ -57,4 +93,22 @@ impl<'a, T> DerefMut for WriteGuard<'a, T> {
     fn deref_mut(&mut self) -> &mut Self::Target {
         unsafe { &mut *self.lock.value.get() }
     }
+}
+
+impl<T> Drop for WriteGuard<'_, T> {
+    fn drop(&mut self) {
+        // Free the lock
+        self.lock.state.store(0, Ordering::Release);
+        // First wake a potential waiting writer
+        self.lock.writer_beacon.fetch_add(1, Ordering::Release);
+        wake_one(&self.lock.writer_beacon);
+        // Then wake all waiting readers
+        wake_all(&self.lock.state);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn rwtest() {}
 }
